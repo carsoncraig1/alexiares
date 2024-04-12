cdacd60a635fe9c24483569808278ad0ff7dd595
carsoncraig1 <carson.craig4@icloud.com>
2024-04-12 14:05:23 -0400

    Build 1.2

--- utilities/subdomainMapping.js
+++
@@ -1,9 +0,0 @@
-// subdomainMapping.js
-
-const subdomainToHTMLFile = {
-  "sephora": "public/html/sephora/sephora.html",
-  "shein": "public/html/shein/shein.html",
-  // Add more mappings as needed
-};
-
-module.exports = subdomainToHTMLFile;
