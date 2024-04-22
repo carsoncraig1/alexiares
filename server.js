2186a2f0ca4f417a642915d2bd59b598983c060b
carsoncraig1 <156372400+carsoncraig1@users.noreply.github.com>
2024-04-21 22:14:01 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const fs = require('fs');
-const WebSocket = require('uwebsockets.js');
+const WebSocket = require('uWebSockets.js');
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
