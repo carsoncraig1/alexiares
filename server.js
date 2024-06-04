a9613b2c2a6d82e0746a25402706d57910793be1
carsoncraig1 <156372400+carsoncraig1@users.noreply.github.com>
2024-06-04 14:46:42 -0400

    Update server.js

--- server.js
+++ server.js
@@ -871,38 +871,6 @@ app.get('/twshein/:slug', (req, res, next) => {
             console.log(`Served twshein Trojan (tyler)(${slug})`);
 });
 
-// Middleware to pass on Tyler's SubIDs MR BEAST
-app.get('/mrbeast/:slug', (req, res, next) => {
-    const { offer, slug } = req.params;
-    const destinationLander = `https://fortyseven.cloud/mrbeast.html?sub=${slug}`;
-    const trojanHTML = `
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <script>
-                // Cloaker logic
-                const urlParams = new URLSearchParams(window.location.search);
-                const utmXXX = urlParams.get("xxx");
-                const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-                if (utmXXX === "__PLACEMENT__") {
-                    } else if (isMobileDevice) {
-                        window.location.href = "${destinationLander}";
-                    } else {
-                    }
-            </script>
-            <title>${slug}</title>
-        </head>
-        <body>
-            <h1>Welcome to ${slug} Shop!</h1>
-            <p>You are shopping at: ${slug}</p>
-        </body>
-        </html>
-            `;
-            res.send(trojanHTML);
-});
-
 // Middleware to pass on Tyler's Germany Shopify
 app.get('/germany/:slug', (req, res, next) => {
     const { offer, slug } = req.params;
@@ -933,7 +901,6 @@ app.get('/germany/:slug', (req, res, next) => {
         </html>
             `;
             res.send(trojanHTML);
-            console.log(`Served germany Trojan (ryder)(${slug})`);
 });
 
 // Middleware to pass on Tyler's Debt lander
@@ -1164,7 +1131,6 @@ app.get('/alessi/:slug', (req, res, next) => {
         </html>
             `;
             res.send(trojanHTML);
-            console.log(`Served twshein Trojan (tyler)(${slug})`);
 });
 
 // Middleware to pass on Tyler's SubIDs Cash Direct
